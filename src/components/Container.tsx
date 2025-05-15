266260a9fae6cb3293e2c097934383f0ff526430
Vishal Gupta <ims.vishal@gmail.com>
2025-05-15 19:18:45 +0530

    Added react-router

---
+++ src/components/Container.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import Maths from "./pages/Maths";
+
+const Container: React.FC = () => {
+  return (
+    <div>
+      <HashRouter>
+        <Routes>
+          <Route path="/maths" element={<Maths />} />
+          <Route path="/" element={<Navigate replace to="/maths" />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
+};
+
+export default Container;
---
+++ src/components/pages/Maths.tsx
@@ -0,0 +1,7 @@
+interface MathsProps {}
+
+const Maths: React.FC = () => {
+  return <div></div>;
+};
+
+export default Maths;
