c7c297991c416caa72ef8574a47709c273754eae
Vishal Gupta <ims.vishal@gmail.com>
2025-05-20 19:03:28 +0530

    Added math operations

--- src/components/pages/Maths.tsx
+++ src/components/pages/Maths.tsx
@@ -1,49 +1,25 @@
 import React, { useState } from "react";
 import { MathOperation } from "../constants/math.enum";
-import Button from "../atoms/Button";
+import MathSetup from "../molecules/MathSetup";
+import MathAssess from "../molecules/MathAssess";
 
 const Maths: React.FC = () => {
   const [started, setStarted] = useState<boolean>(false);
-  const [operation, setOperation] = useState<string>(MathOperation.Addition);
-  const [tpq, setTpq] = useState<number>(Infinity);
-
-  const time = Array.from({ length: 20 }, (_, k) => (k ? 5 * k : 1));
+  const [params, setParams] = useState({
+    operation: MathOperation.Addition,
+    timeLimit: 5
+  });
 
   return (
     <div>
       {started ? (
-        <div>test</div>
+        <MathAssess params={params} />
       ) : (
-        <div>
-          <div>
-            Choose your adventure:
-            <select
-              value={operation}
-              onChange={(e) => setOperation(e.target.value)}
-            >
-              {Object.keys(MathOperation).map((o) => {
-                return <option value={MathOperation[o]}>{o}</option>;
-              })}
-            </select>
-          </div>
-
-          <div>
-            Time limit (in minutes):
-            <select
-              value={tpq.toString()}
-              onChange={(e) => setTpq(parseInt(e.target.value))}
-            >
-              <option value={Infinity}>{Infinity}</option>
-              {time.map((t) => (
-                <option value={t}>{t}</option>
-              ))}
-            </select>
-          </div>
-
-          <div>
-            <Button label="Start" onClick={() => setStarted(true)} />
-          </div>
-        </div>
+        <MathSetup
+          params={params}
+          setParams={setParams}
+          setStarted={setStarted}
+        />
       )}
     </div>
   );
