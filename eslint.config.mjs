1ecf55b9630d03e4a3e65fa8b45512ee64249190
Vishal Gupta <ims.vishal@gmail.com>
2025-09-06 20:41:59 +0530

    Updated project icons

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@next/next/no-img-element": "off"
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
